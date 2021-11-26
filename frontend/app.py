de43213f60de825acbef312dacd151b1a323ef1f
hinatha <hinatha0110@gmail.com>
2021-11-26 22:01:41 +0900

    completed create task

--- frontend/app.py
+++ frontend/app.py
@@ -9,6 +9,8 @@ load_dotenv()
 app = Flask(__name__)
 
 backend_url = os.getenv('BACKEND_URL', 'http://localhost:5000')
+create_url = os.getenv('CREATE_URL', 'http://localhost:5000')
+
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -21,15 +23,16 @@ def hello():
 @app.route("/createtask", methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        
-
+        task = request.form.get('task')
+        detail = request.form.get('detail')
+        payload = {
+            "task":task,
+            "detail":detail
+        }
+        r = requests.post(create_url, json=payload)
         return redirect('/')
-
     else:
         return render_template('create.html')
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
