7c38afc46b64226adac57abd58dda9ffea789e40
sbk0716 <uhey0716@gmail.com>
2021-12-04 08:15:36 +0900

    #1 update index method

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from dotenv import load_dotenv
 import requests
-import ast
-
 import os
 
 load_dotenv()
@@ -16,12 +14,11 @@ task_url = f'''{backend_url}/tasks'''
 @app.route("/")
 def index():
     r = requests.get(task_url)
-    l = ast.literal_eval(r.text)
-    l_task = [d.get("task") for d in l]
-    l_id = [d.get("task_id")for d in l]
-    return render_template('index.html', l_task=l_task, l_id=l_id)
+    r.raise_for_status()
+    tasks = r.json()
+    return render_template('index.html', tasks=tasks)
 
-@app.route("/createtask")
+@app.route("/create-task")
 def create():
     return render_template('create.html')
 
