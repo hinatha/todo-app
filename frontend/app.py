23d4243b8c9b2d6e5bd0f57a60ac3025064901d5
hinatha <hinatha0110@gmail.com>
2021-12-08 13:38:20 +0900

    finished repairing fontend/backend

--- frontend/app.py
+++ frontend/app.py
@@ -1,40 +1,54 @@
 from flask import Flask, render_template, request, redirect
 from dotenv import load_dotenv
 import requests
-import ast
-
 import os
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField
 
 load_dotenv()
 
 app = Flask(__name__)
 
-backend_url = os.getenv('BACKEND_URL', 'http://localhost:5000')
+# What is this for? I'm not sure about this function.
+app.config['SECRET_KEY'] = '123456789'
+
+backend_url = os.getenv("BACKEND_URL", "http://localhost:5000")
 task_url = f'''{backend_url}/tasks'''
 
+# Set task form to add task
+class TaskForm(FlaskForm):
+    task = StringField(validators=[DataRequired()])
+    detail = StringField(validators=[DataRequired()])
+    submit = SubmitField()
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     r = requests.get(task_url)
-    l = ast.literal_eval(r.text)
-    l_task = [d.get("task") for d in l]
-    l_id = [d.get("task_id")for d in l]
-    return render_template('index.html', l_task=l_task, l_id=l_id)
+    r.raise_for_status()
+    tasks = r.json()
+    return render_template("index.html", tasks=tasks)
 
-@app.route("/createtask")
+@app.route("/create-task", methods=["GET"])
 def create():
-    return render_template('create.html')
+    form = TaskForm()
+    return render_template("create.html", form=form)
 
-@app.route("/tasks", methods=['POST'])
-def add():
-    task = request.form.get('task')
-    detail = request.form.get('detail')
-    payload = {
-        "task":task,
-        "detail":detail
-    }
-    requests.post(task_url, json=payload)
-    return redirect('/')
+@app.route("/tasks", methods=["POST"])
+def add_task():
+    print("execute add_task method")
+    form = TaskForm()
+
+    if form.validate_on_submit():
+        task = request.form.get("task")
+        detail = request.form.get("detail")
+        payload = {
+            "task":task,
+            "detail":detail
+        }
+        r = requests.post(task_url, json=payload)
+        r.raise_for_status()
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
