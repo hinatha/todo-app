016958a4cf0f6a535e9c19c4319ede5bcf87a613
hinatha <hinatha0110@gmail.com>
2021-12-09 16:59:35 +0900

    Add comment

--- frontend/app.py
+++ frontend/app.py
@@ -4,24 +4,32 @@ import requests
 import os
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, HiddenField
 
 load_dotenv()
 
 app = Flask(__name__)
 
+# Enable CSRF measures
 app.config["SECRET_KEY"] = "123456789"
 app.config["WTF_CSRF_ENABLED"] = True
 
+# Difine API url
 backend_url = os.getenv("BACKEND_URL", "http://localhost:5000")
 task_url = f"""{backend_url}/tasks"""
 
-# Set task form to add task
+
 class TaskForm(FlaskForm):
+    # For CSRF measures
+    hidden_field_1 = HiddenField("HiddenField 1")
+    hidden_field_2 = HiddenField("HiddenField 2")
+    hidden_field_3 = HiddenField("HiddenField 3")
+    # Set task form to add task
     task = StringField(validators=[DataRequired()])
     detail = StringField(validators=[DataRequired()])
-    submit = SubmitField()
+    submit = SubmitField("Resister")
 
+# Display top page
 @app.route("/", methods=["GET"])
 def index():
     r = requests.get(task_url)
@@ -33,10 +41,12 @@ def index():
 @app.route("/create-task", methods=["GET", "POST"])
 def create():
     form = TaskForm()
+    # Display create page
     if request.method == "GET":
         print("alive")
         return render_template("create.html", form=form)
     else:
+        # Send form to server
         if form.validate_on_submit():
             task = request.form.get("task")
             detail = request.form.get("detail")
@@ -47,6 +57,7 @@ def create():
             r = requests.post(task_url, json=payload)
             r.raise_for_status()
             return redirect("/")
+        # In case of error
         else:
             return render_template("create.html", form=form)
 
